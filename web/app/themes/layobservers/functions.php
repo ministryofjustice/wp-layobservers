93b618892e28e08e2518892e87b3538132508348
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2022-09-08 20:27:24 +0100

    Emergency Banner

--- web/app/themes/layobservers/functions.php
+++ web/app/themes/layobservers/functions.php
@@ -23,6 +23,7 @@ $roots_includes = array(
 	'lib/comments.php', // Custom comments modifications
 	'lib/scripts.php', // Scripts and stylesheets
 	'lib/extras.php', // Custom functions
+  'lib/emergency-banner-settings.php',
 	'lib/plugins.php', // Required plugins
 	'/lib/custom.php', // Custom functions
 	'/lib/meta-box-array.php',  // Meta box array
